27e0a62167f7481d79b50f8254d71c9d7323e51c
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-08-28 20:14:39 -0400

    Create stringslicingreverse.py

---
+++ stringslicingreverse/stringslicingreverse.py
@@ -0,0 +1,18 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+# backwards = letters[::-1]
+# print(backwards)
+
+qpo = letters[16:13:-1]
+print(qpo)
+edcba = letters[4::-1]
+print(edcba)
+last8char = letters[:17:-1]
+print(last8char)
+last8char1 = letters[:-9:-1]
+print(last8char1)
+print(letters[-4:])
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])
+
