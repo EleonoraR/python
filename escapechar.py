371a0bb4e3f229ed1a1c0659ff742b948f7f2c3c
Eleonora <61467087+EleonoraR@users.noreply.github.com>
2023-08-30 22:18:52 -0400

    Create escapechar.py

---
+++ escapechar.py
@@ -0,0 +1,27 @@
+splitString = "This is string has been\nsplit over\nseveral\ntimes"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+print("""The pet shop owner said "No, no,\
+ 'e's uh,...he's resting".""")
+
+anotherSplitString = """This string has been 
+split over 
+several 
+lines"""
+
+anotherSplitString1 = """This string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitString)
+print(anotherSplitString1)
+
+print("C:\\Users\\tella\\notes.txt")
+print(r"C:\Users\tella\notes.txt")
